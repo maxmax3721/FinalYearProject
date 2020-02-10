dca4bc21b9a2e1b7725340f9516110dc35ed110f
maxmax3721 <41957373+maxmax3721@users.noreply.github.com>
2020-02-10 14:26:58 +0000

    10/02/2020

--- Python/Plotting/app.py
+++ Python/Plotting/app.py
@@ -3,14 +3,14 @@ from flask import render_template
 from datetime import time
 import pandas as pd
 import numpy
-
+import os
 
 app = Flask(__name__)
 
 @app.route("/IV")
 def IV():
     #read csv datafile and convert to dataframe
-    Data = pd.read_csv (r'C:\Users\maxbo\OneDrive - University of Bristol\Documents\Year 3\Project\Python\Chart Example edited\data.csv')
+    Data = pd.read_csv (os.getcwd()+('\\data.csv'))
     # sort dataframe into accending voltage values
     Data = Data.sort_values(by=['Voltage'])
     #convert dataframe to separate lists
@@ -21,7 +21,6 @@ def IV():
     xyData = [{'x': int(Voltages[i]), 'z': int(Currents[i])} for i in range(len(Voltages))]
     xyDatastring = str(xyData).replace('z', 'y')
     xyDatastring = str(xyDatastring).replace('\'', '')
-    print(xyDatastring)
     return render_template('IV.html', xydata= xyDatastring , title=title)
 
 if __name__ == "__main__":
