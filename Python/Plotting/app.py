89b2cfbc8928fce92676c2c6a13f1e4077d4d65f
maxmax3721 <41957373+maxmax3721@users.noreply.github.com>
2020-03-07 11:25:22 +0000

    Chart updates instead of creating new chart. Everything work! no call to serial read script yet

--- Python/Plotting/app.py
+++ Python/Plotting/app.py
@@ -10,19 +10,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    #read csv datafile and convert to dataframe
-    Data = pd.read_csv (os.getcwd()+('\\data.csv'))
-    # sort dataframe into accending voltage values
-    Data = Data.sort_values(by=['Voltage'])
-    #convert dataframe to separate lists
-    Currents= Data['Current'].to_numpy()
-    Voltages= Data['Voltage'].to_numpy()
+    
     title= 'IV'
-    #Converts lists to json format to be used in template
-    xyData = [{'x': Voltages[i], 'z': Currents[i]} for i in range(len(Voltages))]
-    xyDatastring = str(xyData).replace('z', 'y')
-    xyDatastring = str(xyDatastring).replace('\'','')
-    return render_template('IV.html', IVData= xyDatastring, title=title)
+
+    return render_template('IV.html', title=title)
 
 @app.route("/Data", methods = ['GET'])
 def Data():
@@ -36,7 +27,7 @@ def Data():
     Voltages= Data['Voltage'].to_numpy()
     title= 'IV'
     #Converts lists to json format to be used in template
-    xyData = [{'x': Voltages[i], 'y': Currents[i]+random.random()} for i in range(len(Voltages))]
+    xyData = [{'x': Voltages[i], 'y': Currents[i]+50*random.random()} for i in range(len(Voltages))]
     xyDatastring = str(xyData).replace('z', 'y')
     xyDatastring = str(xyDatastring).replace('\'','')
     
